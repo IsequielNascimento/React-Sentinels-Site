1228803510ec5f42e6c40fedbc84ec4db3a753a8
IsequieNascimento <isequielnascimento@gmail.com>
2023-01-29 14:17:11 -0300

    Completion of the Info Card at Initiative page

--- src/components/InfoCard/styles.ts
+++ src/components/InfoCard/styles.ts
@@ -2,9 +2,21 @@ import styled from "styled-components";
 import React from "react";
 
 export const InfoContainer = styled.div`
-    width: 600px;
-    height: 400px;
-    background-color: #191A1E;
-    border: 1px solid red;
-    
-`
+  width: 600px;
+  height: 400px;
+  background-color: #191a1e;
+  border: 1px solid red;
+  padding-top: 16px;
+  padding-left: 16px;
+  text-align: left;
+`;
+
+export const Text = styled.p`
+  text-indent: 16px;
+  font-size: 22px;
+  font-weight: normal;
+  margin-right: 8px;
+  margin-left: 8px;
+  white-space: pre-line;
+  line-height: 1.1;
+`;
